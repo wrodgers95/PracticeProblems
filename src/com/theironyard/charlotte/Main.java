cbaf23bbc9e73815f8ad12a32d721009f1e04823
wrodgers95 <will.rodgers1995@gmail.com>
2016-08-21 13:29:03 -0400

    making tests

--- src/com/theironyard/charlotte/Main.java
+++ src/com/theironyard/charlotte/Main.java
@@ -86,4 +86,16 @@ public class Main {
         return 0;
     }
 
+    public static int intMax(int a, int b, int c) {
+
+        int max;
+        if (a > b) {
+            max = a;
+        } else {
+            max = b;
+        } if (c > max) {
+            max = c;
+        } return max;
+    }
+
 }
---
+++ test/com/theironyard/charlotte/MainTest.java
@@ -0,0 +1,69 @@
+package com.theironyard.charlotte;
+
+public class MainTest {
+    @org.junit.Test
+    public void main() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void notString() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void monkeyTrouble() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void sumDouble() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void diff21() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void parrotTrouble() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void makes10() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void nearHundred() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void posNeg() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void startHi() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void icyHot() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void close10() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void intMax() throws Exception {
+
+    }
+
+}
